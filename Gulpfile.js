fbc66d2b8120bae272de27d8713517115a2a1992
Kiril Vatev <vatev.1@gmail.com>
2015-09-07 13:52:39 -0400

    added watch, zip only for production

--- Gulpfile.js
+++ Gulpfile.js
@@ -22,18 +22,26 @@ var epoch = Date.now();
 var version = package.version + '.' + epoch;
 
 var BuildDest = './build';
+var JSSource = [
+    'main.js',
+    'notifications.js',
+    'script.js'
+];
+var HTMLSource = '*.html';
+var ManifestSource = 'manifest.json';
+var AssetsSource = './assets/**';
 
 gulp.task('clean', function() {
     return del([ path.join(BuildDest, '**') ]);
 });
 
 gulp.task('assets', function() {
-    return gulp.src('./assets/**')
+    return gulp.src(AssetsSource)
         .pipe(gulp.dest( path.join(BuildDest, 'assets') ));
 });
 
 gulp.task('manifest', function() {
-    return gulp.src('manifest.json')
+    return gulp.src(ManifestSource)
         .pipe(each(function(content, file, cb) {
             var manifest = JSON.parse(content);
             manifest.version = version;
@@ -42,30 +50,37 @@ gulp.task('manifest', function() {
         .pipe(gulp.dest(BuildDest));
 });
 gulp.task('html', function() {
-    gulp.src('*.html').pipe(gulp.dest(BuildDest));
+    gulp.src(HTMLSource).pipe(gulp.dest(BuildDest));
 });
 
 gulp.task('js', function() {
-    return gulp.src([
-            'main.js',
-            'notifications.js',
-            'script.js'
-        ])
+    return gulp.src(JSSource)
         .pipe(gulpif(production, stripdebug()))
         .pipe(gulpif(production, uglify()))
         .pipe(gulp.dest(BuildDest));
 });
 
 gulp.task('zip', ['assets', 'js', 'html', 'manifest'], function() {
-    var filename = package.name + 'v' + version + '.zip';
+    var filename = package.name + '.v' + version + '.zip';
     
     return gulp.src(path.join(BuildDest, '**'))
         .pipe(zip(filename))
         .pipe(gulp.dest(BuildDest));
 });
 
-gulp.task('build', ['assets', 'js', 'html', 'manifest', 'zip']);
+gulp.task('build', ['assets', 'js', 'html', 'manifest'], function() {
+    if (production) {
+        return gulp.start('zip');
+    }
+});
 
 gulp.task('default', ['clean'], function() {
     return gulp.start('build');
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch(JSSource, ['js']);
+    gulp.watch(HTMLSource, ['html']);
+    gulp.watch(ManifestSource, ['manifest']);
+    gulp.watch(AssetsSource, ['assets']);
 });
