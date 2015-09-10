ba36c5e5583bc5fd19ca81b38e6f8b753eeae1e2
Kiril Vatev <vatev.1@gmail.com>
2015-09-09 20:45:23 -0400

    new versioning system
    Chrome app versioning rules are stupid

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,7 +21,54 @@ var production = (function() {
 
 // set version number to include build time
 var epoch = Date.now();
-var version = package.version + '.' + epoch;
+
+var Version = function(verStr) {
+    var arr = verStr.split('.');
+    
+    this.major = arr[0] || undefined;
+    this.minor = arr[1] || undefined;
+    this.majorRevision = arr[2] || undefined;
+    this.minorRevision = arr[4] || undefined;
+    
+    this.toString = function() {
+        var tempArr = [this.major, this.minor, this.majorRevision, this.minorRevision];
+        var arr = [];
+        var temp = tempArr.shift();
+        
+        while(temp !== undefined) {
+            arr.push(temp);
+            temp = tempArr.shift();
+        }
+        
+        return arr.join('.');
+    };
+};
+
+var calculateVersion = function(verStr) {
+    
+    function pad(num, size) {
+        size = size || 2;
+        var s = num + "";
+        while (s.length < size) { s = "0" + s; }
+        return s;
+    }
+    
+    var ver = new Version(verStr);
+    var date = new Date();
+    var year = date.getUTCFullYear().toString().substr(2);
+    var julian = getJulian(date);
+    var hour = date.getUTCHours();
+    var min = pad(date.getUTCMinutes(), 2);
+    
+    ver.majorRevision = '' + year + julian;
+    ver.minorRevision = parseInt('' + hour + min);
+    
+    return ver.toString();
+};
+
+var version = calculateVersion(package.version).toString();
+
+console.log(version);
 
 var BuildDest = './build';
 var JSSource = [
@@ -87,3 +134,34 @@ gulp.task('watch', ['default'], function() {
     gulp.watch(ManifestSource, ['manifest']); //.on('change', function(ev) { console.log('manifest', ev.path); });
     gulp.watch(AssetsSource, ['assets']); //.on('change', function(ev) { console.log('assets', ev.path); });
 });
+
+
+// TODO this does not really belong in the file
+function isLeap(y){
+    if (y%400 === 0) return true;
+    else if (y%100 === 0) return false;
+    else if (y%4 === 0) return true;
+    else return false;
+}
+
+function getJulian(date){
+    var day = date.getUTCDate(),
+        month = date.getUTCMonth() + 1, //month starts at 0
+        year = date.getUTCFullYear(),
+        doy = 0;
+
+    switch(month) {
+        case 1:
+            doy = day;
+            break;
+        case 2:
+            doy = day + 31;
+            break;
+        default:
+            doy = Math.floor(30.6*month - 91.4) + day; //floor(30.6 (m + 1)) + d - 122
+            doy += (isLeap(year)) ? 60:59;
+            break;
+    }
+
+    return doy; // date on egg cartons starts at 1 (or maybe 0)
+}
