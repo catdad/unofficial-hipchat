ef2894d3e283726f10ab4f1b347d4ba7dfc7d0b0
Kiril Vatev <vatev.1@gmail.com>
2016-05-02 21:49:55 -0400

    opening links from the loader, automatically showing troubleshooting if loader is open for too long
    #5

--- app-loader.js
+++ app-loader.js
@@ -5,6 +5,24 @@ window.addEventListener('load', function() {
     var HIDDEN = 'hide';
     var loader = $('#loader');
     var webview = $('#chat');
+    var troubleshoot = $('#troubleshoot');
+    var links = $.allIn('a', troubleshoot);
+    var close = $('#hide-loader');
+    
+    function kill(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+    }
+    
+    function open(link) {
+        return function opener(ev) {
+            kill(ev);
+            
+            chrome.browser.openTab({
+                url: link.href
+            });
+        };
+    }
     
     function show() {
         loader.classList.remove(HIDDEN);
@@ -14,12 +32,26 @@ window.addEventListener('load', function() {
         loader.classList.add(HIDDEN);
     }
     
+    function showTroughleshootingEventually() {
+        setTimeout(function() {
+            troubleshoot.classList.add('show');
+        }, 1000 * 15);
+    }
+    
     webview.addEventListener('contentload', function() {
         if (/\/chat$/.test(webview.src)) {
             hide();
         }
     });
     
+    close.addEventListener('click', hide);
+    
+    $.forEach(links, function(link) {
+        link.addEventListener('click', open(link));
+    });
+    
     $.on('showLoader', show);
     $.on('hideLoader', hide);
+    
+    showTroughleshootingEventually();
 });
